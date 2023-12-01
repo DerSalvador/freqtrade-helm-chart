1e1a86ea7dc21b477c3eae86100237e5aea5a831
Michael Santos <msantana@swisscom.com>
2023-12-01 02:48:00 +0100

    new FT Version 2023.10

--- chart/extra_strategies/GodStraNew_SMAonly.py
+++ chart/extra_strategies/GodStraNew_SMAonly.py
@@ -4,6 +4,7 @@
 # freqtrade hyperopt --hyperopt-loss SharpeHyperOptLoss --spaces buy roi trailing sell --strategy GodStraNew
 # --- Do not remove these libs ---
 from freqtrade import data
+# from freqtrade.strategy import CategoricalParameter, DecimalParameter
 from freqtrade.strategy import CategoricalParameter, DecimalParameter
 
 from numpy.lib import math
