f97e43f05f2543a6f76320c5fcee1a08938bb67e
Michael Santos <msantana@swisscom.com>
2024-06-08 01:11:28 +0200

    Michaels-MBP.lan-/Users/msantana/dersalvador/freqtrading/freqtrade-helm-chart, Branch: main - Michaels-MBP.lan-/Users/msantana/dersalvador/freqtrading/freqtrade-helm-chart, Branch: main

---
+++ chart/deployed_strategies/bot-ssc-05-k8s-namespace/Anomaly README.md test_pickle.py
@@ -0,0 +1,4 @@
+kubectl --context=gke_vaulted-gift-406223_europe-west1-b_private-cluster-3 -n bot-ssc-05 exec -it pod/freqtrade-bot-ssc-05-8567c779fd-t948m -c freqtrade -- cat /freqtrade/user_data/strategies/Anomaly README.md test_pickle.py
+cat: /freqtrade/user_data/strategies/Anomaly: Is a directory
+cat: README.md: No such file or directory
+cat: test_pickle.py: No such file or directory
